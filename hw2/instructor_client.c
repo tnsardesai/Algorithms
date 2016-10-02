488a27ad1ecf2b441a9cbd2f9871fcf3bc85f80c
tnsardesai <tanmay.sardesai3@gmail.com>
2016-10-01 22:46:01 -0500

    Created a bunch of different sloppy test cases

--- hw2/instructor_client.c
+++ hw2/instructor_client.c
@@ -49,6 +49,65 @@ list arrayToList(int arr[], int sz)  {
 }
 
 
+int test_sublist_ofb(list A){
+    printf("\n\n--------------------------------------\n");            
+    printf("Testing sublist (for lists of ints).\n\n");
+	printList(A);
+	int arr_pos[] = {3,10,6,4};	
+	list pos_list = arrayToList(arr_pos, 4);
+        printf("List with positions pos_list:");
+	printList(pos_list);
+	        
+    list res = sublist(A, pos_list);
+	printf("LIST new list:");
+	printList(res);
+	destroyList(res);
+	
+	destroyList(pos_list);
+	return 0;
+}
+
+int test_deleteOccurrences_no(list A){
+    printf("\n\n--------------------------------------\n");            
+	printf("Test deleteOccurrences (for lists of ints).\n\n");
+	printf("LIST A:");
+	printListHoriz(A);
+
+	deleteOccurrences(A, 600);
+	printListHoriz(A);
+
+	printf("\n----------  end test function -------------------\n");
+	return 0;
+}
+
+int test_insertAtPosition_ne(list A){
+    printf("\n\n--------------------------------------\n");            
+	printf("Test insertAtPosition (for lists of ints).\n\n");
+	printf("LIST A:");
+	printListHoriz(A);
+
+	insertAtPosition(A, 12, -10);
+
+	printListHoriz(A);
+
+	printf("\n----------  end test function -------------------\n");
+	return 0;
+}
+
+int test_insertAtPosition_ge(list A){
+    printf("\n\n--------------------------------------\n");            
+	printf("Test insertAtPosition (for lists of ints).\n\n");
+	printf("LIST A:");
+	printListHoriz(A);
+
+	insertAtPosition(A, 12, 50);
+        
+	printListHoriz(A);
+
+	printf("\n----------  end test function -------------------\n");
+	return 0;
+}
+
 int test_sublist(list A)  {
     printf("\n\n--------------------------------------\n");            
     printf("Testing sublist (for lists of ints).\n\n");
@@ -117,9 +176,42 @@ int test_moveAllMaxAtEnd(list A) {
 }
 
 int main() {
+    /*
+     * The best way to test different cases will be to comment out every
+     * test below except one and run the code with different arrays.
+     * For A=NULL comment out every array and newList().
+     * For A=newList() comment out every array and NULL.
+     * 
+     * For out of bounds on sublist. Run test_sublist_ofb(A)
+     * 
+     * For doesn't occur in A on deleteOccurences. Run test_deleteOccurrences_no(A);
+     * 
+     * For negative position on insert. Run test_insertAtPosition_ne(A); 
+     * 
+     * For greater than length position on insert. Run test_insertAtPosition_ge(A);
+     * 
+     * Without any changes it will run the tests for the array 
+     * which is on homework web site.
+     */
     list A = NULL;
-    int arr[] = {15, 100, 7, 5, 100, 7, 30};	
+    
+    //list A = newList();
+    
+    int arr[] = {15, 100, 7, 5, 100, 7, 30};
     A = arrayToList(arr, 7);
+    
+    //int arr[] = {7,7,7}
+    //A = arrayToList(arr, 3);
+    
+    //int arr[] = {7}
+    //A = arrayToList(arr, 7);
+    
+    //int arr[] = {100, 100, 7, 5, 100, 7, 30};
+    //A = arrayToList(arr, 7);
+    
+    //int arr[] = {7, 100, 7, 5, 100, 7, 30};
+    //A = arrayToList(arr, 7);
+    
     //A = buildListOfInts() ;  // use this line and redirect input to run test cases from data files. See posted example data file.
     printf("LIST A:");
     printList(A);
@@ -127,7 +219,10 @@ int main() {
     test_sublist(A);    
     test_deleteOccurrences(A);     
     test_insertAtPosition(A); 
-    test_moveAllMaxAtEnd(A);   
+    test_moveAllMaxAtEnd(A); 
+    
+    //test_sublist_ofb(A);
+    //test_deleteOccurrences_no(A);
     
     destroyList(A);  
     
